5923f63deb9e5aa00cc96f026404e9d0f5da03c9
cmorrue <carmorrue@gmail.com>
2023-06-09 10:12:30 +0200

    Ultimo Commit

---
+++ bd/signup.go
@@ -0,0 +1,31 @@
+package bd
+
+import (
+	"fmt"
+
+	"github.com/cmorrue/gambituser/models"
+	"github.com/cmorrue/gambituser/tools"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func SignUp(sig models.SignUp) error {
+	fmt.Println("Comienza Registro")
+
+	err := DbConnect()
+	if err != nil {
+		return err
+	}
+	defer Db.Close()
+	sentencia := "INSERT INTO users (User_Email, User_UUID, User_DateAdd,) VALUES ('" + sig.UserEmail + "','" + sig.UserUUID + "','" + tools.FechaMySQL() + "')"
+	fmt.Println(sentencia)
+
+	_, err = Db.Exec(sentencia)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	fmt.Println("SignUp > Ejecución Exitosa")
+	return nil
+
+}
